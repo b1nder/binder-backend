4e4b086b663b900f41357f0bac03bdd85d30e036
yeonnnnjs <sungyeon52@gmail.com>
2024-07-27 18:28:41 +0900

    init

---
+++ src/main/java/com/meteor/binder/BinderApplication.java
@@ -0,0 +1,13 @@
+package com.meteor.binder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinderApplication.class, args);
+	}
+
+}
